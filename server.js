8d92696910a08b257287f5624e4f76c502c63b47
theLoker <bonfantidaniele94@gmail.com>
2020-01-11 17:55:40 +0100

    Rename index in server

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ var app = express();
 var path = require('path');
 var port = 8000;
 
+app.use(express.static(path.join(__dirname, '/')));
+
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/lineUpChecker.html'));
+    res.redirect('lineUpChecker.html');
 });
 
 app.listen(port, function () {
