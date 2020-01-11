e24cc93512bedf6c8a3baaf37b665bd7df2fc4c8
theLoker <bonfantidaniele94@gmail.com>
2020-01-11 23:00:23 +0100

    Validate line up champions + championship

--- js/lineUpChecker.js
+++ js/lineUpChecker.js
@@ -1,11 +1,18 @@
 var titolari;
 var panchinari;
+var cambi;
 
 function lineUpCheck() {
     var serieALineUp = document.getElementById("SerieALineUp").value;
     var championsCupLineUp = document.getElementById("ChampionsCupLineUp").value;
     if(serieALineUp && championsCupLineUp) {
-        validateLineUpComplete(serieALineUp, championsCupLineUp);
+        if(validateLineUpComplete(serieALineUp, championsCupLineUp)) {
+            failLabel.setAttribute("hidden", true);
+            successLabel.removeAttribute("hidden");
+        } else {
+            successLabel.setAttribute("hidden", true);
+            failLabel.removeAttribute("hidden"); 
+        }
     } else if (serieALineUp) {
         if(validateLineUp(serieALineUp)) {
             failLabel.setAttribute("hidden", true);
@@ -26,9 +33,48 @@ function validateLineUpComplete(serieALineUp, championsCupLineUp) {
         var titolariSerieA = titolari;
         var panchinariSerieA = panchinari;
         if(validateLineUp(championsCupLineUp)) {
-
+            var cambiSerieA = new Array();
+            var cambiChampions = new Array();
+            titolariSerieA.forEach(element => {
+                if(element == element.toUpperCase()) {
+                    cambiSerieA.push(element);
+                }
+            });
+            titolari.forEach(element => {
+                if(element == element.toUpperCase()) {
+                    cambiChampions.push(element);
+                }
+            });
+            if(cambiSerieA.length == 3 && cambiChampions.length == 3) {
+                var isValidLineUp = true;
+                var titolariUpperCase;
+                var panchinariUpperCase;
+                cambiSerieA.forEach(element => {
+                    titolariUpperCase = titolari.map(element => element.toUpperCase());
+                    panchinariUpperCase = panchinari.map(element => element.toUpperCase());
+                    if(titolariUpperCase.includes(element) || panchinariUpperCase.includes(element)) {
+                        isValidLineUp = false;
+                    }
+                });
+                cambiChampions.forEach(element => {
+                    titolariSerieAUpperCase = titolariSerieA.map(element => element.toUpperCase());
+                    panchinariUpperCase = panchinariSerieA.map(element => element.toUpperCase());
+                    if(titolariSerieAUpperCase.includes(element) || panchinariUpperCase.includes(element)) {
+                        isValidLineUp = false;
+                    }
+                });
+                return isValidLineUp;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
         }
+    } else {
+        return false;
     }
+
+    return true;
 }
 
 function validateLineUp(lineUp) {
